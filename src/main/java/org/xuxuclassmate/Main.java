f5aceeca078056810b3cdf8ad877375c0a304143
旭旭同学 <xuxuclassmate@gmail.com>
2022-07-06 09:59:24 +0800

    Update Main.java

--- src/main/java/org/xuxuclassmate/Main.java
+++ src/main/java/org/xuxuclassmate/Main.java
@@ -1,4 +1,6 @@
-package org.xuxuclassmate;public class Main {
+package org.xuxuclassmate;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
