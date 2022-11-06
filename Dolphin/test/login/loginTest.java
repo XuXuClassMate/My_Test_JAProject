26d37c473a93f55a93c030251c94fd5e54526ba7
旭旭同学 <xuxuclassmate@gmail.com>
2022-11-06 19:26:53 +0800

    delete test  function

--- Dolphin/test/login/loginTest.java
+++ Dolphin/test/login/loginTest.java
@@ -8,15 +8,6 @@ public class loginTest {
     String Base_url="http://www.xuxuclassmate.xyz:12345/dolphinscheduler";
     login login = new login();
     @Test
-    public void testLogin() throws IOException {
-
-        String path="/login";
-        String result = login.login(Base_url+path);
-        System.out.println(result);
-    }
-
-
-    @Test
     public void TestLoginpost() throws IOException {
         String path="/login";
         String result = login.login2(Base_url+path,"admin","admin123");
