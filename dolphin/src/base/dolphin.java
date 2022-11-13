f719d70e244bac3d166f14cf31fbdefab64b7d9e
旭旭同学 <xuxuclassmate@gmail.com>
2022-11-13 20:27:25 +0800

    add login funstion and login test

---
+++ dolphin/src/base/dolphin.java
@@ -0,0 +1,45 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+package base;
+
+import io.restassured.RestAssured;
+
+import static org.hamcrest.Matchers.equalTo;
+
+public class dolphin {
+    public String logIn(){
+        return RestAssured.given().log().all().
+                queryParam("userName",dolphinConfig.getInstance().userName)
+                .queryParam("userPassword",dolphinConfig.getInstance().passWord)
+                .post(dolphinConfig.getInstance().baseUrl+"/login")
+                .then().log().all().statusCode(200).body("code",  equalTo(0)).extract().path("sessionId");
+
+    }
+    public String logIn(String baseUrl,String userName,String passWord){
+        return RestAssured.given().log().all().
+                queryParam("userName",userName)
+                .queryParam("userPassword",passWord)
+                .post(baseUrl+"/login")
+                .then().log().all().statusCode(200).body("code",  equalTo(0)).extract().path("sessionId");
+
+    }
+
+}
---
+++ dolphin/test/base/dolphinTest.java
@@ -0,0 +1,23 @@
+package base;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
+
+class dolphinTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void logIn() {
+        dolphin dolphin=new dolphin();
+        String sessionId = dolphin.logIn();
+        System.out.println("sessionId : " + sessionId);
+        assertThat(sessionId,not(equalTo(null)));
+    }
+}
