145e021333aaf80cc58d88586de036956e6bd183
XuXuClassMate <xuxuclassmate@gmail.com>
2022-12-25 14:58:34 +0800

    Update Restful.java set json file

--- dolphin/test/page/project/projectTest.java
+++ dolphin/test/page/project/projectTest.java
@@ -23,7 +23,6 @@ package page.project;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-
 import static org.hamcrest.Matchers.equalTo;
 
 
@@ -88,6 +87,5 @@ class projectTest {
                 project.delete(projectCode);
             }
         }
-
     }
 }
